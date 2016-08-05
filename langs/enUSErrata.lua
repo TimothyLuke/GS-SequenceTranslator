001af8d2cd644f476f4b6ee65c76de90a720a7ad
Timothy Minahan <timothy@minahan.net>
2016-08-05 17:57:09 +0930

    #2 one more build to go

--- langs/enUSErrata.lua
+++ langs/enUSErrata.lua
@@ -5,4 +5,5 @@ language[GSTRStaticHash]["enUS"] = {
 	["Ice Flows"] = 108839,
   ["Lightning Bold"] = 8246,
   ["Eathern Shield Totem"] = 198838,
+	["Savage Defense"] = 132402, -- FOr some reason this comes out as ZZOld
 }
